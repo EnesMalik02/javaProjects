7a38aebfcfb6c1bd0dfad570fd6e4032689b98d6
EnesMalik02 <89933069+EnesMalik02@users.noreply.github.com>
2023-10-07 00:24:29 +0300

    Update Avg.java

--- Patika_Arrays/Arrays/src/harmonicAvg/Avg.java
+++ Patika_Arrays/Arrays/src/harmonicAvg/Avg.java
@@ -2,6 +2,7 @@ package harmonicAvg;
 
 public class Avg {
     public static void main(String[] args) {
+        //Listenin içindeki değerleri değiştirerek istediğiniz listenin Harmonik Ortalamasını bulabilirsiniz
         int[] list = {2, 4, 6, 8, 10};
         double sum = 0.0;
         for (int i = 0; i < list.length; i++) {
